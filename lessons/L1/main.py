b68437022aac3370ceff233eabaa7643d2294d52
Alan <alantessier97@gmail.com>
2021-05-31 16:19:38 -0400

    ch1 done

---
+++ lessons/L1/main.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest import TestCase
+from ff import FieldElement
+
+
+class TestFields(TestCase):
+    def test_eq(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(2, 31)
+        c = FieldElement(15, 31)
+        self.assertEqual(a, b)
+        self.assertTrue(a != c)
+        self.assertFalse(a != b)
+
+    def test_add(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(15, 31)
+        self.assertEqual(a + b, FieldElement(17, 31))
+        a = FieldElement(17, 31)
+        b = FieldElement(21, 31)
+        self.assertEqual(a + b, FieldElement(7, 31))
+
+    def test_pow(self):
+        a = FieldElement(17, 31)
+        self.assertEqual(a**3, FieldElement(15, 31))
+
+    def test_sub(self):
+        a = FieldElement(29, 31)
+        b = FieldElement(4, 31)
+        self.assertEqual(a - b, FieldElement(25, 31))
+        a = FieldElement(15, 31)
+        b = FieldElement(30, 31)
+        self.assertEqual(a - b, FieldElement(16, 31))
+
+
+if __name__ == '__main__':
+    unittest.main()
